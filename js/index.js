8c698f5dd5f3337adce4906b8247e2927afd91a5
Wendy <misswendymoira@gmail.com>
2022-04-12 16:10:10 -0700

    interactions started

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+
+function topScroll() {
+document.body.scrollTop = 0;
+}
+
+
+
+function aboutScroll() {
+    const abtscr = document.getElementById("scrollToAbout");
+    abtscr.scrollIntoView();
+}
+
+function workScroll() {
+    const wrkscr = document.getElementById("scrollToWork");
+    wrkscr.scrollIntoView();
+}
+
+
+function flag4Scroll() {
+    const fl4scr = document.getElementById("scrollToFlag4");
+    fl4scr.scrollIntoView();
+}
+
+function contactScroll() {
+    const cntscr = document.getElementById("scrollToContact");
+    cntscr.scrollIntoView();
+}
+
+function aboutOn() {
+    document.getElementById("aboutOverlay").style.display = "block";
+  }
+  
+  function aboutOff() {
+    document.getElementById("aboutOverlay").style.display = "none";
+  }
